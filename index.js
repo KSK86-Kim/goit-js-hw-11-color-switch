64066e7376ae03faae4f3598f653d97f6109679e
Oleksandr Polishchuk <70407461+KSK86-Kim@users.noreply.github.com>
2021-03-06 13:12:10 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+let timer = null;
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function colorChange() {
+    bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+}
+
+function timerHandler(event) {
+    switch (event.target.dataset.action) {
+        case 'start':
+            startBtnRef.disabled = true;
+            timer = setInterval(colorChange, 1000);
+            break;
+        case 'stop':
+            startBtnRef.disabled = false;
+            console.log('stop');
+            clearInterval(timer);
+            break;
+    }
+}
+
+startBtnRef.addEventListener('click', timerHandler);
+stopBtnRef.addEventListener('click', timerHandler);
